4ec914a43446ecbc3b77ddc10651331b19348f0b
Michael Watson <michael@dcpolicycenter.org>
2017-11-17 13:39:28 -0500

    updating py script

--- src/salary_equivilant.py
+++ src/salary_equivilant.py
@@ -1,32 +1,80 @@
 from datetime import *
 from dateutil import rrule
 import pandas as pd
+import json
 import matplotlib.pyplot as plt
 
-def weeks_between(start_date, end_date):
-    weeks = rrule.rrule(rrule.WEEKLY, dtstart=start_date, until=end_date)
+def weeks_between(start_date, end_date,direction):
+    if direction == 'until':
+        weeks = rrule.rrule(rrule.WEEKLY, dtstart=start_date, until=end_date)
+    elif direction == 'from':
+        weeks = rrule.rrule(rrule.WEEKLY, dtstart=end_date, until=start_date)
+    else: weeks = -1
     return weeks.count()
+
+def off_goal(goal,current):
+    difference = goal-current
+    return difference
+
+def required_hourly_rate(goal,current,bill_weekly_hours,weeks_left):
+    diff = off_goal(goal,current)
+    req_rate = diff/(bill_weekly_hours*weeks_left)
+    return(req_rate)
+
+# Date variables
+today = date.today()
 now = datetime.now()
+end_of_year = date(now.year,12,31)
+beginning_of_year = date(now.year,1,1)
+
+profile = pd.read_csv('../data/profile')
+
+goal_salary = profile['goal']
+sched_vac = 10
+sched_sick = 5
+
+days_left = 0
+
+
+total_week_hours = 0
+cumulative_hours = 0
+total_week_bill = 0
+cumulative_bill = 0
+
+
+current_week_index = 0
+
+difference = off_goal(profile['goal'],profile['cumulative_bill'])
+
+print('You are $%f off your goal'%(difference))
+
 week_list = []
 income_list = []
 bill_rate = 40.00
 bill_weekly_hours = 30
 
 
-today = date.today()
-future = date(now.year,12,31)
-one_year = date(now.year+1,now.month,now.day)
-weeks = weeks_between(today,one_year)
 
+
+
+one_year = date(now.year+1,now.month,now.day)
+current_week_index = weeks_between(today,beginning_of_year,'from')
+weeks = weeks_between(today,end_of_year,'until')
+weeks = weeks-1
+
+req_rate = required_hourly_rate(profile['goal'],profile['cumulative_bill'],bill_weekly_hours,weeks)
+
+print('We are in week: %s , there are %s weeks until the end of the year. \n This adds to %s'%(str(current_week_index),str(weeks),str(current_week_index+weeks)))
 
 print("Todays date is %s, there are %s weeks until December 31, %s"%(str(today),str(weeks),str(now.year)))
-previous_income = 0.00
+previous_income = profile['cumulative_bill']
 
 for i in range(weeks):
-    cumulative_income = previous_income + (bill_rate * bill_weekly_hours)
-    print('\n----\nWeek: %s \nCumulative income: $%s\n----\n'%(str(i+1),str(cumulative_income)))
+    cumulative_income = previous_income + (req_rate * bill_weekly_hours)
+    week_count = i + current_week_index + 1
+    print('\n----\nWeek: %s \nCumulative income: $%s\n----\n'%(str(week_count),str(cumulative_income)))
     income_list.append(cumulative_income)
-    week_list.append(i+1)
+    week_list.append(week_count)
     previous_income = cumulative_income
 
 
@@ -34,7 +82,8 @@ for i in range(weeks):
 
 pay = bill_rate * bill_weekly_hours * weeks
 
-print(pay)
+print('%f'%round(req_rate,2))
 
 plt.plot(week_list,income_list)
+plt.plot('52',profile['goal'],'.')
 plt.show()
