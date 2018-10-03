e72c6c63c9aae9c745c05410ea76e1f29655e933
M-Watson <MWatson10@gmail.com>
2018-10-03 10:57:28 -0400

    notebook for exploration

--- nobel/nobel_scrape.py
+++ nobel/nobel_scrape.py
@@ -1,12 +1,25 @@
 import requests
-
+from bs4 import BeautifulSoup as bs
 
 phys_prize_list_url = "https://www.nobelprize.org/prizes/lists/all-nobel-prizes-in-physics/"
 
 page_request = requests.get(phys_prize_list_url)
 
-page_request_content = page_request.text
+page_request_content = page_request.content
+page_request_text = page_request.text
 with open("dump.txt",'w') as f:
-	f.write(page_request_content)
+	f.write(page_request_text)
+
+
+soup = bs(page_request_text)
+all_divs = soup.findAll('div')
+div_by_year = soup.findAll('div', {"class": "by_year"})
+
+for div in div_by_year:
+	with open("dumpdiv.txt",'a') as f:
+		output = str(div) + '\n'
+		f.write(output)
+
+
 
 print("done")
